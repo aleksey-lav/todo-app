cf50cde8bb99e57330ccc0c1b8a73ebe0c0e54c3
aleksey leichenko <ol.leichenko@gmail.com>
2019-01-18 01:19:25 +0200

    static style for todo

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDom from 'react-dom';
 import AppHeader from './components/app-header';
 import SearchPanel from './components/search-panel';
 import TodoList from './components/todo-list';
+import ItemStatusFilter from './components/item-status-filter';
 
 
 // const TodoList = () => {
@@ -27,11 +28,15 @@ const App = () => {
 
     const login = <span>Log in please</span>;
     return (
-        <div>
-            <span>{(new Date().toString())}</span>
-            <br/><br/>{login}
-            <AppHeader/>
-            <SearchPanel/>
+        <div className="todo-app">
+            {/*<span>{(new Date().toString())}</span>*/}
+            {/*<br/><br/>{login}*/}
+            <AppHeader toDo={1} done={3}/>
+            <div className="top-panel d-flex">
+                <SearchPanel/>
+                <ItemStatusFilter/>
+            </div>
+
             <TodoList todos={todoData}/>
         </div>
     );
